a942e0a88bbce2543a1778cd47236278548120b8
DongGeon Lee <donggeon.lee@keymedia.com>
2020-05-26 23:20:01 +1000

    adjust conf

--- conf-sample.js
+++ conf-sample.js
@@ -110,18 +110,18 @@ c.therock.secret = 'YOUR-SECRET'
 // Optional stop-order triggers:
 
 // sell if price drops below this % of bought price (0 to disable)
-c.sell_stop_pct = 0
+c.sell_stop_pct = 0.5
 // buy if price surges above this % of sold price (0 to disable)
-c.buy_stop_pct = 0
+c.buy_stop_pct = 0.3
 // enable trailing sell stop when reaching this % profit (0 to disable)
-c.profit_stop_enable_pct = 0
+c.profit_stop_enable_pct = 12
 // maintain a trailing stop this % below the high-water mark of profit
 c.profit_stop_pct = 1
 
 // Order execution rules:
-
+c.neutral_rate = 'auto'
 // avoid trading at a slippage above this pct
-c.max_slippage_pct = 5
+c.max_slippage_pct = 2
 // buy with this % of currency balance (WARNING : sim won't work properly if you set this value to 100)
 c.buy_pct = 99
 // sell with this % of asset balance (WARNING : sim won't work properly if you set this value to 100)
@@ -129,9 +129,9 @@ c.sell_pct = 99
 // ms to adjust non-filled order after
 c.order_adjust_time = 5000
 // avoid selling at a loss below this pct set to 0 to ensure selling at a higher price...
-c.max_sell_loss_pct = 99
+c.max_sell_loss_pct = 0.3
 // avoid buying at a loss above this pct set to 0 to ensure buying at a lower price...
-c.max_buy_loss_pct = 99
+c.max_buy_loss_pct = 0.3
 // ms to poll order status
 c.order_poll_time = 5000
 // ms to wait for settlement (after an order cancel)
